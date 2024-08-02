94803e635f4ff1c3133d80727759a67698d08dbe
Becca <rebecca_waterson@brown.edu>
2024-08-02 12:22:33 -0700

    fix 2

--- witchy-warthog/src/firebaseConfig.ts
+++ witchy-warthog/src/firebaseConfig.ts
@@ -5,12 +5,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+
+  // @ts-ignore
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+
+// @ts-ignore
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+
+// @ts-ignore
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+
+// @ts-ignore
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+
+// @ts-ignore
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+// @ts-ignore
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
