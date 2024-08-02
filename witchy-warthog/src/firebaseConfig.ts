a7aec1b6eef71716eb653ada09b18e7353ccc80b
Becca <rebecca_waterson@brown.edu>
2024-08-02 12:12:20 -0700

    fix?

--- witchy-warthog/src/firebaseConfig.ts
+++ witchy-warthog/src/firebaseConfig.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
